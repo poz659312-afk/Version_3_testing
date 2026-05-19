67900463706ee5ad1a04d7db5bbfd138769ffa08
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2026-05-19 16:41:58 +0300

    Update navigation.tsx

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -359,8 +359,8 @@ export default function Navigation() {
                     onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
                     className="rounded-full shadow-sm"
                   >
-                    <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-primary" />
+                    <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-primary" />
                     <span className="sr-only">Toggle theme</span>
                   </Button>
                 </div>
